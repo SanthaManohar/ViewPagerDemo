4339883f3121da9a6a00537b94337e9109bb1b45
SanthaManohar <49111237+SanthaManohar@users.noreply.github.com>
2019-12-01 22:38:20 -0800

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,24 @@
+package basicandroid.com.viewpagerdemo;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    ViewPager viewPager;
+    DemoFragmentCollectionAdapter adapter;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewPager = (ViewPager)findViewById(R.id.viewpager);
+        adapter = new DemoFragmentCollectionAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+
+    }
+}
